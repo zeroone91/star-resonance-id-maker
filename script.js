4b05d220f61e619300c1b43cc82d36a812612cdb
zeroone91 <zero.one.game91@gmail.com>
2025-12-07 03:28:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -448,19 +448,19 @@ btnShareX.addEventListener("click", () => {
 async function drawPreview() {
   const design = designSelect.value;
 
-  if (design === "blueprotocol") {
-    canvas.width  = BP_W;
-    canvas.height = BP_H;
+  if (design === "classic") {
+    await drawClassic();
+  } else if (design === "blueprotocol") {
+    await drawBlueprotocol();
   } else {
-    canvas.width  = CANVAS_W;
+    canvas.width = CANVAS_W;
     canvas.height = CANVAS_H;
   }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   if (design === "classic") {
-    canvas.width = 1600;
-    canvas.height = 1200;
-} else if (design === "blueprotocol") {
+    await drawClassic();
+  } else if (design === "blueprotocol") {
     await drawBlueprotocol();
   } else {
     await drawSimple();
